b01b7e093274b298aa8546ac4b1615cf610f8e92
jez115 <zhangkx31@gmail.com>
2025-09-20 18:32:58 -0400

    cal

--- backend/Event.java
+++ backend/Event.java
@@ -1,4 +1,5 @@
 package backend;
+
 import java.util.Date;
 
 public class Event{
--- calendar/calendar/src/Events.java
+++ calendar/calendar/src/Events.java
@@ -3,34 +3,47 @@ import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import backend.Event;
 
 public class Events extends JPanel{
     private static final long serialVersionUID = 1L;  
         public Events(){
+
+            ArrayList<Event> events = new ArrayList<>();
             setLayout(new BorderLayout(20,20));
             setBackground(Color.white);
             setBorder(BorderFactory.createEmptyBorder(40,20,30,20));
 
-            JPanel list = new JPanel(new GridLayout(4,1,10,10));
+            int rows = 4;
+            if (events.size()>4)
+                rows = events.size();
+
+            JPanel list = new JPanel(new GridLayout(rows,1,10,10));
             list.setBackground(Color.white);
 
-            for (int i=0; i<4; i++){
+            JScrollPane sp = new JScrollPane(list);
+
+            for (int i=0; i<events.size(); i++){
             JPanel event = new JPanel(new GridLayout(2,1));
             event.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(10,10,10,10), BorderFactory.createMatteBorder(0,10,0,0, Color.decode("#00d1e8"))));
             event.setBackground(Color.decode("#f0f0f0"));
             event.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
-            JLabel title = new JLabel("Event Title");
+            JLabel title = new JLabel(events.get(i).getName());
             title.setBorder(BorderFactory.createEmptyBorder(0,15,0,15));
             title.setFont(new Font("Noto Sans", Font.PLAIN, 18));
             title.setForeground(Color.black);
             event.add(title);
 
-            JLabel time = new JLabel("Event Time");
+            JLabel time = new JLabel(events.get(i).getDate()+"");
             time.setBorder(BorderFactory.createEmptyBorder(5,15,4,15));
             time.setFont(new Font("Noto Sans", Font.PLAIN, 14));
             time.setForeground(Color.DARK_GRAY);
@@ -38,6 +51,12 @@ public class Events extends JPanel{
 
             list.add(event);
             }
-            add(list, BorderLayout.CENTER);
+            add(sp, BorderLayout.CENTER);
+
+            JButton newEvent = new JButton("New");
+            newEvent.setFont(new Font("Noto Sans", Font.PLAIN, 20));
+            newEvent.setBackground(Color.decode("#00d1e8"));
+            newEvent.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+            add(newEvent, BorderLayout.SOUTH);
         }
     }
