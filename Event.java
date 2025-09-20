e0688fcc7850cc9e717f315f083f6ac5fccf6249
laurenmulcahy17 <lorlor2007@icloud.com>
2025-09-20 12:52:41 -0400

    Create Event.java

---
+++ Event.java
@@ -0,0 +1,12 @@
+public class Event {
+    public String n;
+    public String h;
+    public int d;
+    public int t;
+    public String p;
+    
+    public void Event(String name, String host, int date, int time, String place){
+        n = name;
+
+    }
+}
