05dd6325f6fbd51b1329673d749df038839cb338
jez115 <zhangkx31@gmail.com>
2025-09-20 11:55:38 -0400

    Update Main.java
    name

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("This is Emily");
         System.out.println("This is Lauren");
         System.out.println("This is Lan");
+        System.out.println("This is Jennifer");
     }
 }
