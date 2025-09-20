a3f26a2d7e9fd8726b74e6581cd550f2a625e16a
laurenmulcahy17 <lorlor2007@icloud.com>
2025-09-20 13:21:25 -0400

    updated names

--- Event.java
+++ Event.java
@@ -1,63 +1,63 @@
 public class Event {
     //variables for event
-    public String n;
-    public String h;
-    public int d;
-    public int t;
-    public String p;
+    public String name;
+    public String host;
+    public int date;
+    public int time;
+    public String place;
     public static int eventid;
     
     //constructing new event object
-    public Event(String name, String host, int date, int time, String place){
-        n = name;
-        h = host;
-        d = date; //check how to properly input date
-        t = time;
-        p = place;
+    public Event(String n, String h, int d, int t, String p){
+        name = n;
+        host = h;
+        date = d;
+        time = t;
+        place = p;
         eventid +=1; //check if this is how to make id
     }
 
     //next few methods are just accessors
     public String getName(){
-        return n;
+        return name;
     }
 
     public String getHost(){
-        return h;
+        return host;
     }
 
     public int getDate(){
-        return d;
+        return date;
     }
 
     public int getTime(){
-        return t;
+        return time;
     }
 
     public String getPlace(){
-        return p;
+        return place;
     }
 
 
     //next few methods are to change object variables
-    public void setName(String name){
-        n = name;
+    public void setName(String n){
+        name = n;
     }
 
-    public void setHost(String host){
-        h = host;
+    public void setHost(String h){
+        host = h;
     }
 
-    public void setDate(int date){
-        d = date;
+    public void setDate(int d){
+        date = d;
     }
 
-    public void setTime(int time){
-        t = time;
+    public void setTime(int t){
+        time = t;
     }
 
-    public void setPlace(String place){
-        p = place;
+    public void setPlace(String p){
+        place = p;
     }
 
 
@@ -67,6 +67,6 @@ public class Event {
 //     }
 
     public String toString(){
-        return h + " is hosting " + n + " on " + d + " at " + t + " at " + p;
+        return host + " is hosting " + name + " on " + date + " at " + time + " at " + place;
     }
 }
