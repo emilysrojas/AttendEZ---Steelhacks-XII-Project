da764930d00280fd011704cfa4671df379e60b42
jez115 <zhangkx31@gmail.com>
2025-09-20 16:05:59 -0400

    calendar gui

---
+++ backend/Main.java
@@ -0,0 +1,9 @@
+package backend;
+public class Main {
+    public static void main(String [] args){
+        System.out.println("This is Emily");
+        System.out.println("This is Lauren");
+        System.out.println("This is Lan");
+        System.out.println("This is Jennifer");
+    }
+}
