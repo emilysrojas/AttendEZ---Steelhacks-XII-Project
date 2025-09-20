6401777f9ab6eaf64d95e77f8a1e02ce764592f6
laurenmulcahy17 <lorlor2007@icloud.com>
2025-09-20 13:17:01 -0400

    event class

--- Event.java
+++ Event.java
@@ -1,12 +1,72 @@
 public class Event {
+    //variables for event
     public String n;
     public String h;
     public int d;
     public int t;
     public String p;
+    public static int eventid;
     
-    public void Event(String name, String host, int date, int time, String place){
+    //constructing new event object
+    public Event(String name, String host, int date, int time, String place){
         n = name;
+        h = host;
+        d = date; //check how to properly input date
+        t = time;
+        p = place;
+        eventid +=1; //check if this is how to make id
+    }
 
+    //next few methods are just accessors
+    public String getName(){
+        return n;
+    }
+
+    public String getHost(){
+        return h;
+    }
+
+    public int getDate(){
+        return d;
+    }
+
+    public int getTime(){
+        return t;
+    }
+
+    public String getPlace(){
+        return p;
+    }
+
+
+    //next few methods are to change object variables
+    public void setName(String name){
+        n = name;
+    }
+
+    public void setHost(String host){
+        h = host;
+    }
+
+    public void setDate(int date){
+        d = date;
+    }
+
+    public void setTime(int time){
+        t = time;
+    }
+
+    public void setPlace(String place){
+        p = place;
+    }
+
+
+//     //next method determine point value for event
+//     public int getPoint(){
+//         return 
+//     }
+
+    public String toString(){
+        return h + " is hosting " + n + " on " + d + " at " + t + " at " + p;
     }
 }
